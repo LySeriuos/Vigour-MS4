ee390c7532241a136d056a8e4ff4c8c29baa01e7
LySeriuos <drungilas.p@gmail.com>
2022-01-31 07:32:17 +0000

    Added home app and templates

--- Vigour/urls.py
+++ Vigour/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # all the urls we need for login logout password resets and so on.
-     path('', include('home.urls')),
+    path('', include('home.urls')),
 ]
